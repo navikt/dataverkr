309b3f166ec442fae37b8246a0abdb017c9d02dc
Espen Rosenquist <espen.rosenquist@nav.no>
2020-11-18 14:21:46 +0100

    Functions in separate files, example file referenced for all

---
+++ R/add_fig.R
@@ -0,0 +1,23 @@
+#' Adds plotly fig to datapackage object
+#'
+#' @param dp Datapackage
+#' @param fig Plotly figure
+#' @param title Title of figure
+#' @param description Description of figure
+#' @importFrom plotly plotly_json
+#' @example man/examples/example_dp.R
+#' @export
+add_fig <- function(dp,
+                    fig,
+                    title = NULL,
+                    description = NULL){
+
+  fig_json <- plotly_json(fig)
+
+  dp$add_view(spec = fig_json$x$data,
+              spec_type = 'plotly',
+              title = title,
+              description = description,
+              name = title,
+              resources = list())
+}
---
+++ R/create_dp.R
@@ -0,0 +1,16 @@
+#' Creates a dataverk datapackage object
+#'
+#' @param metadata Metadata
+#' @param python_path Path to python version
+#' @importFrom reticulate use_python import dict
+#' @example man/examples/example_dp.R
+#' @export
+create_dp <- function(metadata,
+                      python_path = system("which python")){
+
+  use_python(python_path)
+  dataverk <- import("dataverk")
+  pyMetadata <- dict(metadata)
+
+  dataverk$Datapackage(pyMetadata)
+}
